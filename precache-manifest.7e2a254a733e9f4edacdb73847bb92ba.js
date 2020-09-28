09b0c7bab75c82b23b81f38ad367e5a473acef33
Tiffany <chin.tiffany108@gmail.com>
2020-09-28 16:28:28 -0400

    Updates

---
+++ precache-manifest.7e2a254a733e9f4edacdb73847bb92ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3fb7499fc98aaa0913ccba2262338b",
+    "url": "/Color-Picker/index.html"
+  },
+  {
+    "revision": "c489d37f3114e62adb65",
+    "url": "/Color-Picker/static/js/2.34aeebb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Color-Picker/static/js/2.34aeebb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8cbc4f74d2ea372a66",
+    "url": "/Color-Picker/static/js/main.5125789a.chunk.js"
+  },
+  {
+    "revision": "1cce43cd6a7e88003e60",
+    "url": "/Color-Picker/static/js/runtime-main.c63eb57a.js"
+  }
+]);
