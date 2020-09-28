cfe7da6ab74e3ebdba428497fb010b41365559fe
Tiffany <chin.tiffany108@gmail.com>
2020-09-28 17:08:36 -0400

    add meta tags

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Color-Picker/precache-manifest.b3533c8592a2ef39011a84fbb48a098e.js"
+  "/Color-Picker/precache-manifest.4d3c26bab0ba38dcd6d0daed047bd208.js"
 );
 
 self.addEventListener('message', (event) => {
